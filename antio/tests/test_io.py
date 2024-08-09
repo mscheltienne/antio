6e4eb76f6e365850ea8b7f446ddd1f8d9daecaf5
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-08-09 17:47:08 +0200

    add test file

---
+++ antio/tests/test_io.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from importlib.resources import files
+from typing import TYPE_CHECKING
+
+import pytest
+from mne.io import read_raw_brainvision
+from numpy.testing import assert_allclose
+
+from antio.io import read_raw_ant
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+@pytest.fixture()
+def datafiles() -> dict[str, Path]:
+    """Return a dict of cnt and brainvision files from the same recording."""
+    cnt = files("antio.tests") / "data" / "cnt" / "test_test_2023-06-07_18-54-58.cnt"
+    bv = (
+        files("antio.tests")
+        / "data"
+        / "brainvision"
+        / "test_test_2023-06-07_18-54-58.vhdr"
+    )
+    return dict(cnt=cnt, bv=bv)
+
+
+def test_io(datafiles):
+    """Test loading of .cnt file."""
+    raw1 = read_raw_ant(datafiles["cnt"])
+    raw2 = read_raw_brainvision(datafiles["bv"])
+    assert_allclose(raw1.get_data(), raw2.get_data())
