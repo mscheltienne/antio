56520e0343253ddd39d355a4cdb735a1324dfc14
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-08-12 10:27:15 +0200

    update build configuration

---
+++ tests/test_io.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from antio.io import read_raw_ant
+
+
+@pytest.fixture()
+def datafiles() -> Path:
+    """Return the path to a .cnt file."""
+    return Path(__file__) / "data" / "test_test_2023-06-07_18-54-58.cnt"
+
+
+def test_io(datafiles):
+    """Test loading of .cnt file."""
+    pytest.importorskip("mne")
+    raw1 = read_raw_ant(datafiles["cnt"])
