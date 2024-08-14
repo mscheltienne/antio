ce311de226014ac1658c7804c6c2af27d10d80ab
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-08-14 16:39:03 +0200

    simpler optional dependency import module

--- src/antio/utils/_imports.py
+++ src/antio/utils/_imports.py
@@ -6,22 +6,13 @@ Inspired from pandas: https://pandas.pydata.org/
 from __future__ import annotations
 
 import importlib
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from types import ModuleType
-    from typing import Optional
 
 # A mapping from import name to package name (on PyPI) when the package name
 # is different.
 _INSTALL_MAPPING: dict[str, str] = {}
 
 
-def import_optional_dependency(
-    name: str,
-    extra: str = "",
-    raise_error: bool = True,
-) -> Optional[ModuleType]:
+def import_optional_dependency(name: str, extra: str = "") -> None:
     """Import an optional dependency.
 
     By default, if a dependency is missing an ImportError with a nice message will be
@@ -33,29 +24,12 @@ def import_optional_dependency(
         The module name.
     extra : str
         Additional text to include in the ImportError message.
-    raise_error : bool
-        What to do when a dependency is not found.
-        * True : Raise an ImportError.
-        * False: Return None.
-
-    Returns
-    -------
-    module : Module | None
-        The imported module when found.
-        None is returned when the package is not found and raise_error is False.
     """
     package_name = _INSTALL_MAPPING.get(name)
     install_name = package_name if package_name is not None else name
 
-    try:
-        module = importlib.import_module(name)
-    except ImportError:
-        if raise_error:
-            raise ImportError(
-                f"Missing optional dependency '{install_name}'. {extra} Use pip or "
-                f"conda to install {install_name}."
-            )
-        else:
-            return None
-
-    return module
+    if importlib.util.find_spec(name) is None:
+        raise ImportError(
+            f"Missing optional dependency '{install_name}'. {extra} Use pip or "
+            f"conda to install {install_name}."
+        )
--- tests/utils/test_imports.py
+++ tests/utils/test_imports.py
@@ -6,16 +6,14 @@ from antio.utils._imports import import_optional_dependency
 def test_import_optional_dependency():
     """Test the import of optional dependencies."""
     # Test import of present package
-    numpy = import_optional_dependency("numpy")
-    assert isinstance(numpy.__version__, str)
+    import_optional_dependency("numpy")
 
     # Test import of absent package
     with pytest.raises(ImportError, match="Missing optional dependency"):
-        import_optional_dependency("non_existing_pkg", raise_error=True)
+        import_optional_dependency("non_existing_pkg")
 
     # Test import of absent package without raise
-    pkg = import_optional_dependency("non_existing_pkg", raise_error=False)
-    assert pkg is None
+    import_optional_dependency("non_existing_pkg")
 
     # Test extra
     with pytest.raises(ImportError, match="blabla"):
