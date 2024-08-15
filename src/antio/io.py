f6c9e9d04a24fdc3a43a03f2359b1fb4309bad93
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-08-15 16:15:13 +0200

    better structure

--- src/antio/io.py
+++ src/antio/io.py
@@ -1,133 +1,50 @@
 from __future__ import annotations
 
+import importlib
 from typing import TYPE_CHECKING
 
-import numpy as np
-
 if TYPE_CHECKING:
-    from numpy.typing import NDArray
+    from pathlib import Path
 
-    from .libeep import InputCNT
-
-
-def read_info(cnt: InputCNT) -> tuple[list[str], list[str], list[str], list[str]]:
-    """Parse the channel information from the cnt file.
-
-    Parameters
-    ----------
-    cnt : InputCNT
-        The cnt object from which the information is read.
-
-    Returns
-    -------
-    ch_names : list of str
-        List of channel names.
-    ch_units : list of str
-        List of human-readable units for each channel.
-    ch_refs : list of str
-        List of channel reference electrodes.
-    """
-    ch_names, ch_units, ch_refs = [], [], []
-    for k in range(cnt.get_channel_count()):
-        ch_curr = cnt.get_channel(k)
-        ch_names.append(ch_curr[0])
-        ch_units.append(ch_curr[1].lower())  # always lower the unit for mapping
-        ch_refs.append(ch_curr[2])
-    return ch_names, ch_units, ch_refs
+    from mne.io.ant.ant import RawANT
 
 
-def read_data(cnt: InputCNT) -> NDArray[np.float64]:
-    """Read the data array.
+def read_raw_ant(
+    fname: str | Path,
+    eog: str | None = None,
+    misc: str | None = r"BIP\d+",
+    bipolars: list[str] | tuple[str, ...] | None = None,
+    impedance_annotation: str = "impedance",
+    *,
+    verbose=None,
+) -> RawANT:
+    """Reader for Raw ANT files in .cnt format."""
+    if importlib.util.find_spec("mne") is None:
+        raise ImportError("Missing 'mne'. Use pip or conda to install 'mne'.")
 
-    Parameters
-    ----------
-    cnt : InputCNT
-        The cnt object from which the data is read.
+    from mne.utils import check_version
 
-    Returns
-    -------
-    data : array of shape (n_channels, n_samples)
-        The numpy array containing the data, scaled to SI units if able.
-    """
-    n_samples = cnt.get_sample_count()  # sample = (n_channels,)
-    data = cnt.get_samples(0, n_samples)
-    return np.array(data).reshape(n_samples, -1).T  # (n_channels, n_samples)
+    if check_version("mne", "1.9"):
+        raise RuntimeError(
+            "The ANT-Neuro reader was added to MNE-Python 1.9. Either upgrade MNE "
+            "or use 'antio' version 0.1.0 to read the CNT file to a Raw object."
+        )
 
+    from mne.io import read_raw_ant
+    from mne.utils import warn
 
-def read_triggers(
-    cnt: InputCNT, impedance_annotation: str
-) -> tuple[list[int], list[int], list[str], list[list[float]]]:
-    """Read triggers into the attribute of MNE's annotation.
+    warn(
+        "This function is deprecated in favor of mne.io.read_raw_ant() and will be "
+        "removed in the future.",
+        DeprecationWarning,
+        module="antio",
+    )
 
-    Parameters
-    ----------
-    cnt : InputCNT
-        The cnt object from which the triggers are read.
-    impedance_annotation : str
-        The description of the annotation for impedance measurements.
-
-    Returns
-    -------
-    onsets : list of int
-        List of onsets of the triggers, in sample.
-    durations : list of int
-        List of duration of the triggers, in sample.
-    descriptions : list of str
-        List of descriptions of the triggers.
-    impedances : list of list of float
-        List of impedances for each channel, for each impedance trigger.
-    """
-    n_triggers = cnt.get_trigger_count()
-    onsets, durations, descriptions, impedances = [], [], [], []
-    disconnect = dict(start=[], stop=[])
-    for k in range(n_triggers):
-        code, idx, duration, condition, description, impedance = cnt.get_trigger(k)
-        # detect impedance measurements
-        if (
-            description is not None
-            and description.lower() == "impedance"
-            and impedance is not None
-        ):
-            impedances.append([float(elt) for elt in impedance.split(" ")])
-            # create an impedance annotation to mark the measurement
-            onsets.append(idx)
-            durations.append(duration)
-            descriptions.append(impedance_annotation)
-            continue
-        # detect amplifier disconnection
-        if condition is not None and condition.lower() == "amplifier disconnected":
-            disconnect["start"].append(idx)
-            continue
-        elif condition is not None and condition.lower() == "amplifier reconnected":
-            disconnect["stop"].append(idx)
-            continue
-        # treat all the other triggers as regular event annotations
-        onsets.append(idx)
-        durations.append(duration)
-        if description is not None:
-            descriptions.append(description)
-        else:
-            descriptions.append(code)
-    # create BAD_disconnection annotations, don't bother with all the special cases, if
-    # the annotations look weird, just add the bare 9001 and 9002 disconnect/reconnect
-    # annotations.
-    if (
-        len(disconnect["start"]) == len(disconnect["stop"])
-        and len(disconnect["start"]) != 0
-        and disconnect["start"][0] < disconnect["stop"][0]
-        and disconnect["start"][-1] < disconnect["stop"][-1]
-    ):
-        for start, stop in zip(disconnect["start"], disconnect["stop"]):
-            onsets.append(start)
-            durations.append(stop - start)
-            descriptions.append("BAD_disconnection")
-    else:
-        for elt in disconnect["start"]:
-            onsets.append(elt)
-            durations.append(0)
-            descriptions.append("Amplifier disconnected")
-        for elt in disconnect["stop"]:
-            onsets.append(elt)
-            durations.append(0)
-            descriptions.append("Amplifier reconnected")
-    return onsets, durations, descriptions, impedances
+    return read_raw_ant(
+        fname=fname,
+        eog=eog,
+        misc=misc,
+        bipolars=bipolars,
+        impedance_annotation=impedance_annotation,
+        verbose=verbose,
+    )
---
+++ src/antio/parse.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from .libeep import InputCNT
+
+
+def read_info(cnt: InputCNT) -> tuple[list[str], list[str], list[str], list[str]]:
+    """Parse the channel information from the cnt file.
+
+    Parameters
+    ----------
+    cnt : InputCNT
+        The cnt object from which the information is read.
+
+    Returns
+    -------
+    ch_names : list of str
+        List of channel names.
+    ch_units : list of str
+        List of human-readable units for each channel.
+    ch_refs : list of str
+        List of channel reference electrodes.
+    """
+    ch_names, ch_units, ch_refs = [], [], []
+    for k in range(cnt.get_channel_count()):
+        ch_curr = cnt.get_channel(k)
+        ch_names.append(ch_curr[0])
+        ch_units.append(ch_curr[1].lower())  # always lower the unit for mapping
+        ch_refs.append(ch_curr[2])
+    return ch_names, ch_units, ch_refs
+
+
+def read_data(cnt: InputCNT) -> NDArray[np.float64]:
+    """Read the data array.
+
+    Parameters
+    ----------
+    cnt : InputCNT
+        The cnt object from which the data is read.
+
+    Returns
+    -------
+    data : array of shape (n_channels, n_samples)
+        The numpy array containing the data.
+    """
+    n_samples = cnt.get_sample_count()  # sample = (n_channels,)
+    data = cnt.get_samples(0, n_samples)
+    return np.array(data).reshape(n_samples, -1).T  # (n_channels, n_samples)
+
+
+def read_triggers(cnt: InputCNT) -> tuple[list, list, list, list, dict[str, list[int]]]:
+    """Read triggers into the attribute of MNE's annotation.
+
+    Parameters
+    ----------
+    cnt : InputCNT
+        The cnt object from which the triggers are read.
+
+    Returns
+    -------
+    onsets : list of int
+        List of onsets of the trigger in samples.
+    durations : list of int
+        List of durations of the trigger in samples.
+    descriptions : list of str
+        List of descriptions of the trigger.
+    impedances : list of list of float
+        List of impedance measurements, one value per channel.
+    disconnect : dict
+        Dictionary with keys 'start' and 'stop' containing the onsets of the amplifier
+        disconnection and reconnection.
+    """
+    onsets, durations, descriptions, impedances = [], [], [], []
+    disconnect = dict(start=[], stop=[])
+    for k in range(cnt.get_trigger_count()):
+        code, idx, duration, condition, description, impedance = cnt.get_trigger(k)
+        # detect impedance measurements
+        if (
+            description is not None
+            and description.lower() == "impedance"
+            and impedance is not None
+        ):
+            impedances.append([float(elt) for elt in impedance.split(" ")])
+            # create an impedance annotation to mark the measurement
+            onsets.append(idx)
+            durations.append(duration)
+            descriptions.append("impedance")
+            continue
+        # detect amplifier disconnection
+        if condition is not None and condition.lower() == "amplifier disconnected":
+            disconnect["start"].append(idx)
+            continue
+        elif condition is not None and condition.lower() == "amplifier reconnected":
+            disconnect["stop"].append(idx)
+            continue
+        # treat all the other triggers as regular event annotations
+        onsets.append(idx)
+        durations.append(duration)
+        if description is not None:
+            descriptions.append(description)
+        else:
+            descriptions.append(code)
+    return onsets, durations, descriptions, impedances, disconnect
