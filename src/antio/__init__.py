75b1ebc7da023e66cab0a898dddf5836c359a153
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-08-15 19:34:01 +0200

    fix module name

--- src/antio/__init__.py
+++ src/antio/__init__.py
@@ -1,4 +1,4 @@
-from . import datasets, io, libeep, parse, utils
+from . import datasets, io, libeep, parser, utils
 from ._version import __version__
 from .libeep import read_cnt
 from .utils.config import sys_info
