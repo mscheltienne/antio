b9b5474a94048b458af57dd2209e08960ef23f4c
Mathieu Scheltienne <mathieu.scheltienne@dandelion.science>
2025-07-16 10:37:15 +0200

    update tests

--- tests/utils/test_config.py
+++ tests/utils/test_config.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
 from io import StringIO
+
+import pytest
 
 from antio.utils.config import sys_info
 
 
-def test_sys_info():
+def test_sys_info() -> None:
     """Test info-showing utility."""
     out = StringIO()
     sys_info(fid=out)
@@ -20,13 +24,38 @@ def test_sys_info():
     assert "numpy" in value
     assert "psutil" in value
 
-    assert "ruff" not in value
-    assert "pytest" not in value
+    assert "style" not in value
+    assert "test" not in value
 
     out = StringIO()
     sys_info(fid=out, developer=True)
     value = out.getvalue()
     out.close()
 
-    assert "ruff" in value
-    assert "pytest" in value
+    assert "style" in value
+    assert "test" in value
+
+
+def test_sys_info_invalid() -> None:
+    """Test getting information on package without requirements."""
+    out = StringIO()
+    with pytest.raises(RuntimeError, match="could not be retrieved"):
+        sys_info(fid=out, package="numpy")
+    out.close()
+
+
+def test_sys_info_other_package() -> None:
+    """Test getting information on another package."""
+    out = StringIO()
+    sys_info(fid=out, package="psutil")
+    value = out.getvalue()
+    out.close()
+    assert "psutil" in value
+
+
+def test_sys_info_other_package_dev() -> None:
+    """Test getting developer information on another package."""
+    out = StringIO()
+    with pytest.raises(RuntimeError, match="from source in an editable install"):
+        sys_info(fid=out, package="psutil", developer=True)
+    out.close()
