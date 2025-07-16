733ea9d442e8c42e4f7a9baab2fe9fc4a36dae47
Mathieu Scheltienne <mathieu.scheltienne@dandelion.science>
2025-07-16 10:36:24 +0200

    drop python 3.10

--- src/antio/_version.py
+++ src/antio/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.6.0.dev37+g981eaae.d20250716'
-__version_tuple__ = version_tuple = (0, 6, 0, 'dev37', 'g981eaae.d20250716')
+__version__ = version = '0.6.0.dev38+g3e0ee6b.d20250716'
+__version_tuple__ = version_tuple = (0, 6, 0, 'dev38', 'g3e0ee6b.d20250716')
